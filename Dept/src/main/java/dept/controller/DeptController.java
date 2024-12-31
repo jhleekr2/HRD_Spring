c930237d2591f2b8d32d2e4411fd0705ffb63132
jhleekr2 <jhleekr2@hanmail.net>
2025-01-01 01:59:05 +0900

    20250101

---
+++ Dept/src/main/java/dept/controller/DeptController.java
@@ -0,0 +1,13 @@
+package dept.controller;
+
+import org.springframework.stereotype.Controller;
+
+import lombok.extern.slf4j.Slf4j;
+
+//로그 객체를 정의하지 않고 사용하기 위해서 롬복의 @Slf4j annotation을 사용하고
+//기존 logger.info 대신 log.info 형태로 바꿔서 사용한다
+@Controller
+@Slf4j
+public class DeptController {
+
+}
